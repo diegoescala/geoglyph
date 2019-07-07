ec5cea3e75bbe7eb9f60d8db56b1396b4b63a669
Diego Escala <diego.escala@yahoo.com>
2019-07-07 13:01:02 -0500

    Added unit test

--- test/geoglyph/core_test.clj
+++ test/geoglyph/core_test.clj
@@ -6,6 +6,12 @@
   (testing "Binary string to number conversion"
     (is (= (bin-str-to-num "110") 6))))
 
+(deftest test-bin-stream-to-hex
+  (testing "Binary stream to hex conversion"
+    (and
+      (is (= (bin-stream-to-hex "01011110") "5E")
+          (= (bin-stream-to-hex "100101") "9")))))
+
 (deftest test-encode
   (testing "Geoglyph encoding to binary"
     (and (is (= (encode 32.67 90.0 0.01)
