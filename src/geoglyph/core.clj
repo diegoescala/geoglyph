379cd0b80e91202d5b35b48cb84dd61ed92cd1b6
Diego Escala <diego.escala@yahoo.com>
2019-07-06 23:38:09 -0500

    Can now encode to hex

--- src/geoglyph/core.clj
+++ src/geoglyph/core.clj
@@ -8,13 +8,27 @@
                     (int))]
     values))
 
+(defn num-to-hex
+  [num]
+  (let [digits (into {} (map-indexed vector "0123456789ABCDEF"))]
+    (get digits num)))
+
+(defn bin-stream-to-hex
+  [bin]
+  (->> (partition 4 bin)
+       (map #(apply str %))
+       (map bin-str-to-num)
+       (map num-to-hex)
+       (map str)
+       (reduce str)))
+
 (defn encode
   [number max-val accuracy]
   (loop [encoded ""
          pos 0.0
          dir 1.0
          move-amount (* 0.5 max-val)]
-    (println (prn-str [number max-val accuracy encoded pos dir move-amount]))
+    (println (prn-str [number max-val accuracy encoded (bin-stream-to-hex encoded) pos dir move-amount]))
     (if (or (<= 128 (count encoded)) (<= (Math/abs (- pos number)) accuracy))
       encoded
       (let [delta (- number pos)
@@ -29,7 +43,7 @@
                (* -1.0 dir)
                (if (= -1.0 dir) (* 0.5 move-amount) move-amount))))))
 
-(let [s (str (encode 32.19 90 0.001))]
+(let [s (str (encode 25.77 90 0.01))]
   (println (str "Encoded:  " s))
   (println (str "Num bits: " (count s)))
   (println (str "Hex len:  " (/ (count s) 4)))
